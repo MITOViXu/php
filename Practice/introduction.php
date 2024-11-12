8310ae7ffe78fc641f7589756b9d9d4224d27ac3
Theodore Nguyen <theodore.nguyen@avtech.com.au>
2024-11-12 16:30:12 +0700

    feat: complete introduction

---
+++ Practice/introduction.php
@@ -0,0 +1,18 @@
+<h1>Thực hành xây dựng chức năng quản lí người dùng</h1>
+<h2>Phần 1: Xác thực truy cập</h2>
+<ul>
+    <li>Đăng nhập</li>
+    <li>Đăng ký</li>
+    <li>Đăng xuất</li>
+    <li>Quên mật khẩu</li>
+    <li>Kích hoạt tài khoản</li>
+</ul>
+<h2>Phần 2: Quản lí người dùng</h2>
+<ul>
+    <li>Kiểm tra người dùng đăng nhập</li>
+    <li>Thêm người dùng</li>
+    <li>Sửa và xóa người dùng</li>
+    <li>Hiển thị số users</li>
+    <li>Phân trang</li>
+    <li>Tìm kiếm, lọc dữ liệu</li>
+</ul>
