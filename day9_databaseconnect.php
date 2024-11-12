cd58ada646e910603f43c9bfd28209208b921d62
Theodore Nguyen <theodore.nguyen@avtech.com.au>
2024-11-12 14:00:21 +0700

    feat(proj): complete database connection setup

--- day9_databaseconnect.php
+++ day9_databaseconnect.php
@@ -7,12 +7,12 @@
 </head>
 <body>
     <div>
-        <div style="width: 50%; display:inline-block; text-align: center;">
+        <div style="width: 48%; display:inline-block; text-align: center;">
             <h4>Database connect</h4>
             <iframe width="100%" height="500px" src="https://www.youtube.com/embed/WdlKojbG1Xo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <img src="./assets/class_object.png" alt="" style="width: 100%;">
         </div>
-        <div style="display:inline-block; padding:0; vertical-align: top; width:30%;">
+        <div style="display:inline-block; padding:0; vertical-align: top; width:50%;">
             <?php 
                 // Thong tin ket noi   
                 const _HOST = 'localhost';               
@@ -23,17 +23,21 @@
                     if(class_exists('PDO')) {
                         $dsn = 'mysql:dbname='._DB.';'._HOST;
                         $con = new PDO($dsn, _USER, _PASS);
-                        echo'Nếu kết nối thành công sẽ hiện ra: object(PDO)#1 (0) { } <br>';
+                        echo'Nếu kết nối thành công sẽ hiện ra: object(PDO)#1 (0) { } <br><br>';
+                        echo"<h4 style='color: green; font-weight: bold; margin-left: 20px;'> Success: ";
                         var_dump( $con );
+                        echo'<h4>';
                     }
                 }
                 catch (Exception $e) {
-                    echo "Có lỗi";
+                    echo '<div style="color: red; padding: 5px 15px; border: 1px solid red; white-space: pre-wrap; word-wrap: break-word; overflow-wrap: break-word;">';
+                    echo $e;
+                    echo '</div>';
                     die();
                 }
             ?>  
             <h4>Source</h4>
-            <p>
+            <p style="border: 1px solid black; padding: 10px 20px; width:100%;">
                 &lt;?php <br>
                     &nbsp;&nbsp;&nbsp;&nbsp;// Thong tin ket noi<br>  
                     &nbsp;&nbsp;&nbsp;&nbsp;const _HOST = 'localhost';<br>               
